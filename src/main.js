f93f6cd628d0d292cc00343c3978802ef92f3e8c
Chelsea Roston <cmroston@gmail.com>
2021-05-08 16:18:04 -0400

    updates for deploy

--- src/main.js
+++ src/main.js
@@ -11,6 +11,9 @@ import VueSimpleAlert from "vue-simple-alert";
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
+axios.defaults.baseURL = process.env.NODE_ENV === "production" ? "https://desolate-brook-47135.herokuapp.com" : "/";
+
+
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
   axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
